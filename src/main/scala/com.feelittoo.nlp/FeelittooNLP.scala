c754ef68662518086c26f138ddbccd58287b8918
Noppanit Charassinvichai <noppanit.c@gmail.com>
2012-05-13 16:23:08 +0100

    Added gen-idea plugin and make the test pass

---
+++ src/main/scala/com.feelittoo.nlp/FeelittooNLP.scala
@@ -0,0 +1,8 @@
+import edu.stanford.nlp.ie._;
+import edu.stanford.nlp.ie.crf._;
+
+class FeelittooNLP {
+	def getOrganizationName(text :String):List[String] = {
+    return List[String]("Hello")
+	}
+}
--- src/test/scala/com.feelittoo.nlp/FeelittooNLPSpec.scala
+++ src/test/scala/com.feelittoo.nlp/FeelittooNLPSpec.scala
@@ -1,9 +1,11 @@
 import org.specs2.mutable._
+import scala.util._
 
 class FeelittooNLPSpec extends Specification {
 	"NLP Library" should {
 		"return organisation names" in {
-			"Hello" must have size(5)
+			var feelittooNLP = new FeelittooNLP
+			feelittooNLP.getOrganizationName("My iPhone is from Apple") must have size(1)
 		}
 	}
 }
