dddf22a21ef43d256d6705fd9fd744df5ff1ace5
Noppanit Charassinvichai <noppanit.c@gmail.com>
2012-06-10 10:23:24 +0200

    Added getting nouns

--- src/main/scala/com.feelittoo.nlp/FeelittooServlet.scala
+++ src/main/scala/com.feelittoo.nlp/FeelittooServlet.scala
@@ -6,10 +6,13 @@ import net.liftweb.json.JsonAST._
 import net.liftweb.json.Printer._
 import net.liftweb.json.JsonDSL._
 import edu.stanford.nlp.ie.crf.CRFClassifier
+import edu.stanford.nlp.tagger.maxent.MaxentTagger
+import edu.stanford.nlp.ling.{Sentence, TaggedWord}
 
 class FeelittooServlet extends ScalatraServlet with ScalateSupport {
 
   private val classifier = CRFClassifier.getClassifierNoExceptions("./classifiers/english.all.3class.distsim.crf.ser.gz")
+  private val tagger = new MaxentTagger("./models/english-bidirectional-distsim.tagger")
 
   post("/text") {
     val text = params("text")
@@ -17,6 +20,16 @@ class FeelittooServlet extends ScalatraServlet with ScalateSupport {
     val pattern = """(\w+)/ORGANIZATION""".r
 
     val list = pattern.findAllIn(classifier.classifyToString(text)).matchData.map(m => m.group(1)).toList
+
+    pretty(render(list))
+  }
+
+  post("/noun") {
+    val text = params("text")
+    contentType = "application/json"
+    val pattern = """(\w+)/NN""".r
+
+    val list = pattern.findAllIn(tagger.tagString(text)).matchData.map(m => m.group(1)).toList
 
     pretty(render(list))
   }
--- src/test/scala/com.feelittoo.nlp/FeelittooServletSpec.scala
+++ src/test/scala/com.feelittoo.nlp/FeelittooServletSpec.scala
@@ -4,14 +4,26 @@ import org.scalatra.test.specs2._
 
 // For more on Specs2, see http://etorreborre.github.com/specs2/guide/org.specs2.guide.QuickStart.html 
 class FeelittooServletSpec extends ScalatraSpec { def is =
-  "POST / on FeelittooServlet"                     ^
-    "should return status 200"                  ! root200^
+  "POST / on FeelittooServlet"                          ^
+    "should return status 200 and organisation names"   ! getOrgNames ^
+    "should return status 200 and nouns"                ! getNouns ^
+    "should return status 200 and nouns of long string" ! getNounsOfLongString
                                                 end
     
   addServlet(classOf[FeelittooServlet], "/*")
 
-  def root200 = post("/text", Map("text" -> "I love Google and Apple")) {
+  def getOrgNames = post("/text", Map("text" -> "I love Google and Apple")) {
     status must_== 200
     body must_== "[\"Google\",\"Apple\"]"
+  }
+
+  def getNouns = post("/noun", Map("text" -> "I eat apple")) {
+    status must_== 200
+    body must_== "[\"apple\"]"
+  }
+
+  def getNounsOfLongString = post("/noun", Map("text" -> "The world can be such a scary place, full of trials and tribulations but each day I wake up I know that your face and your morning hugs will be right there waiting for you. There is no other way to describe the word LOVE. To me love means simply means YOU. Many we continue to live the life we deserve. May the good Lord continue to shower us with good life, happiness and health. Happy 16 years anniversary.")) {
+    status must_==  200
+    body must_== "[\"world\",\"place\",\"trials\",\"tribulations\",\"day\",\"face\",\"morning\",\"hugs\",\"way\",\"word\",\"LOVE\",\"love\",\"life\",\"Lord\",\"life\",\"happiness\",\"health\",\"years\",\"anniversary\"]"
   }
 }
